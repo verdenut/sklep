1b39a3eaa5074643ebcaa72142cf405785019744
Wojciech Jablonski <jabwojtek@wp.pl>
2020-11-16 15:42:07 +0100

    Updates

---
+++ precache-manifest.8184c1da4c792f7a8df52cab3d5867a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d31c003a3794a67212",
+    "url": "/sklep/static/css/main.8d8d2b62.chunk.css"
+  },
+  {
+    "revision": "f2d31c003a3794a67212",
+    "url": "/sklep/static/js/main.ead6015d.chunk.js"
+  },
+  {
+    "revision": "49ccb8bcb0b0c4e193bc",
+    "url": "/sklep/static/js/runtime~main.803af992.js"
+  },
+  {
+    "revision": "986323b5c4fcc7465a20",
+    "url": "/sklep/static/css/2.b5c6a97d.chunk.css"
+  },
+  {
+    "revision": "986323b5c4fcc7465a20",
+    "url": "/sklep/static/js/2.ebee49df.chunk.js"
+  },
+  {
+    "revision": "85a0642c6cde8071c546cb1da0d1a3dc",
+    "url": "/sklep/static/media/img1.85a0642c.jpg"
+  },
+  {
+    "revision": "c0d98cc9f77b1f28a2412a100dcc6f47",
+    "url": "/sklep/static/media/canal-5488271.c0d98cc9.jpg"
+  },
+  {
+    "revision": "338b0b472a1c1891f355e0f7098a40c9",
+    "url": "/sklep/static/media/img3.338b0b47.jpg"
+  },
+  {
+    "revision": "2eeff5d87bef93d1778d5ed6d4ca38de",
+    "url": "/sklep/static/media/img4.2eeff5d8.webp"
+  },
+  {
+    "revision": "873b3451443e047ebb090eeb88cc9b18",
+    "url": "/sklep/index.html"
+  }
+];
